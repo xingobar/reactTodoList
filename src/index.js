97af6df0c32add0474cfd66f6cb0c92d5341c9e8
xingobar <gary840227@gmail.com>
2018-08-21 19:17:44 +0700

    1.使用 redux logger

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import todoReducer from './reducers';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import dotenv from 'dotenv';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const middleware = compose(applyMiddleware(thunk), window.devToolsExtension && window.devToolsExtension());
+dotenv.load(); // load env file
 
-let store = createStore(todoReducer, middleware);
+const middleware = [ thunk ];
+
+if (process.env.NODE_ENV !== 'production') {
+	middleware.push(createLogger());
+}
+
+// const middleware = compose(applyMiddleware(thunk), window.devToolsExtension && window.devToolsExtension());
+
+let store = createStore(todoReducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 ReactDOM.render(
 	<Provider store={store}>
